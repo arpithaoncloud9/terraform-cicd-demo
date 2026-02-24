b3459ced36b1acc4ab19488cbd9a3593a2da7cb8
arpithaoncloud9 <arpithaoncloud9@gmail.com>
2026-02-24 18:03:49 -0500

    Tested with different inputs and triggered pipeline

--- main.tf
+++ main.tf
@@ -2,9 +2,19 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "demo" {
-  bucket = var.bucket_name
-  tags = {
-    Environment = "dev"
+
+resource "aws_s3_bucket" "bucket_b" {
+  bucket = "maria-import-demo-2026"
+}
+
+resource "aws_s3_bucket_versioning" "bucket_b_versioning" {
+  bucket = aws_s3_bucket.bucket_b.id
+
+  versioning_configuration {
+    status = "Enabled"
   }
+}
+
+resource "aws_s3_bucket" "bucket_c" {
+  bucket = "maria-import-demo-2027"
 }
